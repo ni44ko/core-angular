19116695ce9ffa71ba413f7ec3d0ab8e2d7a1e18
ni44ko <nikomatutina@gmail.com>
2019-02-24 03:14:38 +0800

    Add project files.

---
+++ Allsop_Cart/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Allsop_Cart.Models
+{
+    public class Product
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int quantity { get; set; }
+        public Category Category { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
